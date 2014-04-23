43afe0e540609c366ab0e8a58691ef631d824b59
Justin Halsall <justin@juice10.com>
2014-04-23 22:26:28 +0200

    call the server correctly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,31 @@
 'use strict';
-
+// var TripUpdate = $resource('/trips/:tripId/update', {tripId:'@id'});
+// angular.module('app', ['ngResource']).provider('Trip', function () {
+//   this.$get = ['$resource', function ($resource) {
+//     var Trip = $resource('http://127.0.0.1:3000/trips/:tripId/update', {tripId:'@id'});
+//     return Trip;
+//   }];
+//
+// });
 angular.module('app')
-  .controller('MainCtrl', function ($scope) {
-    $scope.speed = null;
+  .provider('Trip', function () {
+    this.$get = ['$resource', function ($resource) {
+      var Trip = $resource('http://127.0.0.1:3000/trips/:tripId/:action', {tripId:'@tripToken'}, {
+        updateSpeed: {
+          method:'POST',
+          params: {action: 'update'}
+        }
+      });
+      return Trip;
+    }];
+  })
+  .controller('MainCtrl', function ($scope, Trip) {
+    window.Trip = Trip;
+    $scope.tripToken = null;
+    $scope.songPlaying = null;
 
     var geoSuccess = function(position) {
-      var speed = position.coords.speed || (Math.random() * 10);
-      $scope.speed = speed;
-      $scope.$apply();
+      sendSpeedToServer(position);
     };
 
     var geoError = function(error) {
@@ -15,5 +33,23 @@ angular.module('app')
             'message: ' + error.message + '\n');
     };
 
-    navigator.geolocation.watchPosition(geoSuccess, geoError, { timeout: 30000 });
+    var getTripId = function (callback) {
+      $scope.trip = new Trip();
+      $scope.trip.$save().then(function (response) {
+        callback();
+      });
+    };
+
+    var sendSpeedToServer = function (position) {
+      var trip = $scope.trip;
+      trip.lat = position.coords.latitude;
+      trip.long = position.coords.longitude;
+      trip.speed = position.coords.speed || (Math.random() * 10);
+      $scope.$apply();
+      trip.$updateSpeed();
+    };
+
+    getTripId(function() {
+      navigator.geolocation.watchPosition(geoSuccess, geoError, { timeout: 30000 });
+    });
   });
