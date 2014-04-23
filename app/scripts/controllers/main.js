62125998338ca07ec4a43c228dc9c2aac972d042
Justin Halsall <justin@juice10.com>
2014-04-23 19:31:10 +0200

    cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,16 +4,16 @@ angular.module('beatforspeedMobileApp')
   .controller('MainCtrl', function ($scope) {
     $scope.speed = null;
 
-    this.geoSuccess = function(position) {
+    var geoSuccess = function(position) {
       var speed = position.coords.speed || (Math.random() * 10);
       $scope.speed = speed;
       $scope.$apply();
     };
 
-    this.geoError = function(error) {
+    var geoError = function(error) {
       alert('code: '    + error.code    + '\n' +
             'message: ' + error.message + '\n');
     };
 
-    navigator.geolocation.watchPosition(this.geoSuccess, this.geoError, { timeout: 30000 });
+    navigator.geolocation.watchPosition(geoSuccess, geoError, { timeout: 30000 });
   });
