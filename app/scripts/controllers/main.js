de641fc94e8793dccd9b224aef2cde938d2a71bc
Justin Halsall <justin@juice10.com>
2014-04-23 18:03:56 +0200

    updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,24 @@ angular.module('beatforspeedMobileApp')
       'AngularJS',
       'Karma'
     ];
+    console.log('doing something');
+
+    // onSuccess Callback
+    // This method accepts a Position object, which contains the
+    // current GPS coordinates
+    //
+    function onSuccess(position) {
+      var element = document.getElementById('geolocation');
+      console.log(position);
+      element.innerHTML = 'SPEEED: '  + position.coords.speed;
+    }
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+      alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
   });
